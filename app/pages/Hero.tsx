af1924b87f1af3030bcd9ffaf95870b0954dcd3e
Grenish rai <ariestheracer@gmail.com>
2024-09-26 15:29:23 +0530

    feat: Add Hero component for home page

---
+++ app/pages/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center  background">
+      <h1 className="text-10xl ciko text-white">ATHENA</h1>
+      <p className="text-3xl text-white -m-10 ciko">SMIT's Cultural Festival</p>
+    </div>
+  );
+}
