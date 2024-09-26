4fa49aff121448488bbba490d1f4a6543264190e
Grenish rai <ariestheracer@gmail.com>
2024-09-26 15:27:57 +0530

    chore: Update tailwind.config.ts to add new font size variant

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      fontSize:{
+        "10xl": "10rem",
+      }
     },
   },
   plugins: [],
