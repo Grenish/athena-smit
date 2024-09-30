44464a6edca47237e0ebb1293396f32038554f7b
Grenish rai <ariestheracer@gmail.com>
2024-10-01 04:01:12 +0530

    refactor: Update import path for Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import EventTicketGen from "./pages/EventTickerGen";
-import EventTickerPar from "./pages/EventTicketPar";
 import Hero from "./pages/Hero";
-import Navbar from "./pages/Navbar";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
