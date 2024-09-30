e604b7fa230755ee0803f3c19d3f6c29195aa6d2
Grenish rai <ariestheracer@gmail.com>
2024-10-01 04:02:03 +0530

    refactor: Improve Hero component with lazy loading for images

--- app/pages/Hero.tsx
+++ app/pages/Hero.tsx
@@ -1,11 +1,22 @@
+import Link from "next/link";
+
+
 export default function Hero() {
   return (
     <div className="h-screen flex flex-col justify-center items-center relative overflow-hidden background">
-      <h1 className="text-10xl ciko text-white relative z-10">ATHENA</h1>
-      <p className="text-3xl text-white -m-10 ciko relative z-10">
+      <h1 className="text-10xl ciko text-white relative z-10 pointer-events-none">
+        ATHENA
+      </h1>
+      <p className="text-3xl text-white -m-10 ciko pointer-events-none">
         SMIT's Cultural Festival
       </p>
-      
+      <div className="absolute z-10 bottom-20 group">
+        <Link href="/tickets">
+          <button className="text-white doodles-slant text-5xl capitalize border-2 p-3 rounded-xl group-hover:animate-bounce">
+            Grab your ticket now -{">"}
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
