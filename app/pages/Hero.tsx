265fe1a9954612ce0d021937a261aec6aaa93e53
Grenish rai <ariestheracer@gmail.com>
2024-09-30 23:32:30 +0530

    feat: refractored the code

--- app/pages/Hero.tsx
+++ app/pages/Hero.tsx
@@ -1,8 +1,11 @@
 export default function Hero() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center  background">
-      <h1 className="text-10xl ciko text-white">ATHENA</h1>
-      <p className="text-3xl text-white -m-10 ciko">SMIT's Cultural Festival</p>
+    <div className="h-screen flex flex-col justify-center items-center relative overflow-hidden background">
+      <h1 className="text-10xl ciko text-white relative z-10">ATHENA</h1>
+      <p className="text-3xl text-white -m-10 ciko relative z-10">
+        SMIT's Cultural Festival
+      </p>
+      
     </div>
   );
 }
